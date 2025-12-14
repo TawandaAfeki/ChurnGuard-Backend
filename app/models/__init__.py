c8bc8bb627bc922ca76c424623247f1ed2fa6fd8
TawandaAfeki <afekitawanda@gmail.com>
2025-12-14 21:15:08 +0200

    Alerts

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,5 @@
-from app.database import Base
-
 from .user import User
-from .customer import Customer
-from .alert import Alert
 from .company import Company
+from .client import Client
+from .alert import Alert
+
