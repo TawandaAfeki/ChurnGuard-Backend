d77759a04254e1cf0ebe2b422f49d1b0e9ac6438
TawandaAfeki <afekitawanda@gmail.com>
2025-12-15 20:17:47 +0200

    FIX

--- app/models/client.py
+++ app/models/client.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import relationship
 from app.database import Base
 
 class Client(Base):
-    __tablename__ = "clients"
+    __tablename__ = "customers"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
